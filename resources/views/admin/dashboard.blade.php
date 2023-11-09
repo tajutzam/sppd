b54ca8f5e625c800f72ec53f30de8e995d0673e6
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2023-11-09 21:08:10 +0700

    update frontend

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,98 @@
 @extends('layouts.app')
 <title>Dashboard - SPPD Puskesmas Ketrowonojoyo</title>
 @section('content')
+    <div class="container-fluid">
+        <div class="row page-titles p-0">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item" style="font-weight: bold; font-size: 30px"><a
+                        href="javascript:void(0)">Dashboard</a></li>
+
+            </ol>
+        </div>
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="card tryal-gradient">
+                                    <div class="card-body tryal row">
+                                        <div class="col-xl-7 col-sm-6">
+                                            <h1 style="color:white; font-weight: bold">Selamat Datang</h1>
+                                            <h3 style="color: primary">Di SPPD Puskesmas Ketrowonojoyo</h3>
+                                            <span></span>
+                                            <a href="javascript:void(0);" class="btn btn-rounded-3  fs-18 font-w500">Admin
+                                                1</a>
+                                        </div>
+
+                                        <div class="col-xl-5 col-sm-6">
+                                            <img src="{{ asset('') }}images/joyo.png" alt="" class="sd-shape">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-xl-6">
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="row">
+                                    <div class="col-xl-6 col-sm-6">
+                                        <div class="widget-stat card bg-danger">
+                                            <div class="card-body  p-4">
+                                                <div class="media">
+                                                    <span class="me-3">
+                                                        <i class="flaticon-381-user-7"></i>
+                                                    </span>
+                                                    <div class="media-body text-white text-end">
+                                                        <p class="mb-1">JUMLAH PEGAWAI</p>
+                                                        <h3 class="text-white">76</h3>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-6 col-sm-6">
+                                        <div class="widget-stat card bg-warning">
+                                            <div class="card-body  p-4">
+                                                <div class="media">
+                                                    <span class="me-3">
+                                                        <i class="la la-user"></i>
+                                                    </span>
+                                                    <div class="media-body text-white text-end">
+                                                        <p class="mb-1">JUMLAH KADER</p>
+                                                        <h3 class="text-white">76</h3>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12 col-sm-12">
+                                        <div class="widget-stat card bg-success">
+                                            <div class="card-body  p-4">
+                                                <div class="media">
+                                                    <span class="me-3">
+                                                        <i class="flaticon-381-calendar-1"></i>
+                                                    </span>
+                                                    <div class="media-body text-white text-end">
+                                                        <p class="mb-1">Jumlah SPT</p>
+                                                        <h3 class="text-white">76</h3>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
